30be82281627a2b7ed4ad520b86becac351fdfb1
hsayed21 <hsayedhamed21@gmail.com>
2021-02-03 08:48:46 +0200

    Added some functions

--- Memories/Form1.Designer.cs
+++ Memories/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Memories
+﻿namespace Memories
 {
     partial class Form1
     {
@@ -29,167 +28,182 @@ namespace Memories
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.lblWord = new System.Windows.Forms.Label();
-            this.lblSentence = new System.Windows.Forms.Label();
-            this.lblTranslation = new System.Windows.Forms.Label();
-            this.txtWord = new System.Windows.Forms.TextBox();
-            this.txtSentence = new System.Windows.Forms.TextBox();
-            this.txtTranslation = new System.Windows.Forms.TextBox();
-            this.lblTime = new System.Windows.Forms.Label();
-            this.txtTime = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.txt_translation = new System.Windows.Forms.TextBox();
+            this.lb_word = new System.Windows.Forms.Label();
+            this.lb_sentence = new System.Windows.Forms.Label();
+            this.lb_translation = new System.Windows.Forms.Label();
+            this.txt_sentence = new System.Windows.Forms.TextBox();
+            this.txt_word = new System.Windows.Forms.TextBox();
+            this.remove = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.get = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnStart = new System.Windows.Forms.Button();
-            this.btnGet = new System.Windows.Forms.Button();
-            this.btnRemove = new System.Windows.Forms.Button();
-            this.btnAdd = new System.Windows.Forms.Button();
+            this.lb_time = new System.Windows.Forms.Label();
+            this.txt_time = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // lblWord
+            // add
             // 
-            this.lblWord.AutoSize = true;
-            this.lblWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWord.Location = new System.Drawing.Point(40, 120);
-            this.lblWord.Name = "lblWord";
-            this.lblWord.Size = new System.Drawing.Size(60, 25);
-            this.lblWord.TabIndex = 0;
-            this.lblWord.Text = "Word";
+            this.add.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(350, 414);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(150, 55);
+            this.add.TabIndex = 0;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
             // 
-            // lblSentence
+            // txt_translation
             // 
-            this.lblSentence.AutoSize = true;
-            this.lblSentence.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSentence.Location = new System.Drawing.Point(40, 237);
-            this.lblSentence.Name = "lblSentence";
-            this.lblSentence.Size = new System.Drawing.Size(96, 25);
-            this.lblSentence.TabIndex = 0;
-            this.lblSentence.Text = "Sentence";
+            this.txt_translation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_translation.ForeColor = System.Drawing.Color.Green;
+            this.txt_translation.Location = new System.Drawing.Point(162, 298);
+            this.txt_translation.Name = "txt_translation";
+            this.txt_translation.Size = new System.Drawing.Size(568, 30);
+            this.txt_translation.TabIndex = 1;
+            this.txt_translation.Text = "لكن أنا أريدك - أن تعدّل سلوكك يا سيد";
+            this.txt_translation.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // lblTranslation
+            // lb_word
             // 
-            this.lblTranslation.AutoSize = true;
-            this.lblTranslation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTranslation.Location = new System.Drawing.Point(27, 358);
-            this.lblTranslation.Name = "lblTranslation";
-            this.lblTranslation.Size = new System.Drawing.Size(109, 25);
-            this.lblTranslation.TabIndex = 0;
-            this.lblTranslation.Text = "Translation";
+            this.lb_word.AutoSize = true;
+            this.lb_word.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_word.Location = new System.Drawing.Point(51, 100);
+            this.lb_word.Name = "lb_word";
+            this.lb_word.Size = new System.Drawing.Size(47, 20);
+            this.lb_word.TabIndex = 6;
+            this.lb_word.Text = "Word";
             // 
-            // txtWord
+            // lb_sentence
             // 
-            this.txtWord.Location = new System.Drawing.Point(165, 111);
-            this.txtWord.Multiline = true;
-            this.txtWord.Name = "txtWord";
-            this.txtWord.Size = new System.Drawing.Size(364, 38);
-            this.txtWord.TabIndex = 1;
+            this.lb_sentence.AutoSize = true;
+            this.lb_sentence.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_sentence.Location = new System.Drawing.Point(51, 200);
+            this.lb_sentence.Name = "lb_sentence";
+            this.lb_sentence.Size = new System.Drawing.Size(78, 20);
+            this.lb_sentence.TabIndex = 7;
+            this.lb_sentence.Text = "Sentence";
             // 
-            // txtSentence
+            // lb_translation
             // 
-            this.txtSentence.Location = new System.Drawing.Point(165, 229);
-            this.txtSentence.Multiline = true;
-            this.txtSentence.Name = "txtSentence";
-            this.txtSentence.Size = new System.Drawing.Size(364, 38);
-            this.txtSentence.TabIndex = 1;
+            this.lb_translation.AutoSize = true;
+            this.lb_translation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_translation.Location = new System.Drawing.Point(51, 303);
+            this.lb_translation.Name = "lb_translation";
+            this.lb_translation.Size = new System.Drawing.Size(87, 20);
+            this.lb_translation.TabIndex = 8;
+            this.lb_translation.Text = "Translation";
             // 
-            // txtTranslation
+            // txt_sentence
             // 
-            this.txtTranslation.Location = new System.Drawing.Point(165, 350);
-            this.txtTranslation.Multiline = true;
-            this.txtTranslation.Name = "txtTranslation";
-            this.txtTranslation.Size = new System.Drawing.Size(364, 38);
-            this.txtTranslation.TabIndex = 1;
+            this.txt_sentence.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_sentence.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.txt_sentence.Location = new System.Drawing.Point(162, 195);
+            this.txt_sentence.Name = "txt_sentence";
+            this.txt_sentence.Size = new System.Drawing.Size(568, 30);
+            this.txt_sentence.TabIndex = 9;
+            this.txt_sentence.Text = "I want you to adjust your attitude, mister.";
+            this.txt_sentence.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // lblTime
+            // txt_word
             // 
-            this.lblTime.AutoSize = true;
-            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTime.Location = new System.Drawing.Point(27, 497);
-            this.lblTime.Name = "lblTime";
-            this.lblTime.Size = new System.Drawing.Size(86, 25);
-            this.lblTime.TabIndex = 0;
-            this.lblTime.Text = "Time(s):";
+            this.txt_word.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_word.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.txt_word.Location = new System.Drawing.Point(162, 93);
+            this.txt_word.Name = "txt_word";
+            this.txt_word.Size = new System.Drawing.Size(568, 32);
+            this.txt_word.TabIndex = 10;
+            this.txt_word.Text = "adjust";
+            this.txt_word.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // txtTime
+            // remove
             // 
-            this.txtTime.Location = new System.Drawing.Point(115, 493);
-            this.txtTime.Multiline = true;
-            this.txtTime.Name = "txtTime";
-            this.txtTime.Size = new System.Drawing.Size(103, 38);
-            this.txtTime.TabIndex = 1;
+            this.remove.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.remove.Location = new System.Drawing.Point(1117, 401);
+            this.remove.Name = "remove";
+            this.remove.Size = new System.Drawing.Size(105, 55);
+            this.remove.TabIndex = 11;
+            this.remove.Text = "Remove";
+            this.remove.UseVisualStyleBackColor = true;
+            // 
+            // start
+            // 
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(794, 402);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(126, 54);
+            this.start.TabIndex = 12;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            // 
+            // get
+            // 
+            this.get.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.get.Location = new System.Drawing.Point(953, 402);
+            this.get.Name = "get";
+            this.get.Size = new System.Drawing.Size(126, 54);
+            this.get.TabIndex = 14;
+            this.get.Text = "Get";
+            this.get.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(645, 111);
+            this.dataGridView1.Location = new System.Drawing.Point(794, 88);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(493, 277);
-            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.Size = new System.Drawing.Size(428, 240);
+            this.dataGridView1.TabIndex = 15;
             // 
-            // btnStart
+            // lb_time
             // 
-            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStart.Location = new System.Drawing.Point(645, 485);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(141, 50);
-            this.btnStart.TabIndex = 3;
-            this.btnStart.Text = "Start";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            this.lb_time.AutoSize = true;
+            this.lb_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_time.Location = new System.Drawing.Point(51, 435);
+            this.lb_time.Name = "lb_time";
+            this.lb_time.Size = new System.Drawing.Size(65, 20);
+            this.lb_time.TabIndex = 17;
+            this.lb_time.Text = "Time(s):";
             // 
-            // btnGet
+            // txt_time
             // 
-            this.btnGet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGet.Location = new System.Drawing.Point(839, 485);
-            this.btnGet.Name = "btnGet";
-            this.btnGet.Size = new System.Drawing.Size(133, 50);
-            this.btnGet.TabIndex = 3;
-            this.btnGet.Text = "Get";
-            this.btnGet.UseVisualStyleBackColor = true;
-            // 
-            // btnRemove
-            // 
-            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRemove.Location = new System.Drawing.Point(1012, 483);
-            this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(126, 52);
-            this.btnRemove.TabIndex = 3;
-            this.btnRemove.Text = "Remove";
-            this.btnRemove.UseVisualStyleBackColor = true;
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(304, 479);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(141, 50);
-            this.btnAdd.TabIndex = 3;
-            this.btnAdd.Text = "Add";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.txt_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_time.ForeColor = System.Drawing.Color.Olive;
+            this.txt_time.Location = new System.Drawing.Point(122, 430);
+            this.txt_time.Name = "txt_time";
+            this.txt_time.Size = new System.Drawing.Size(89, 30);
+            this.txt_time.TabIndex = 18;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1289, 574);
-            this.Controls.Add(this.btnRemove);
-            this.Controls.Add(this.btnGet);
-            this.Controls.Add(this.btnAdd);
-            this.Controls.Add(this.btnStart);
+            this.ClientSize = new System.Drawing.Size(1346, 526);
+            this.Controls.Add(this.txt_time);
+            this.Controls.Add(this.lb_time);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.txtTime);
-            this.Controls.Add(this.txtTranslation);
-            this.Controls.Add(this.txtSentence);
-            this.Controls.Add(this.txtWord);
-            this.Controls.Add(this.lblTime);
-            this.Controls.Add(this.lblTranslation);
-            this.Controls.Add(this.lblSentence);
-            this.Controls.Add(this.lblWord);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Controls.Add(this.get);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.remove);
+            this.Controls.Add(this.txt_word);
+            this.Controls.Add(this.txt_sentence);
+            this.Controls.Add(this.lb_translation);
+            this.Controls.Add(this.lb_sentence);
+            this.Controls.Add(this.lb_word);
+            this.Controls.Add(this.txt_translation);
+            this.Controls.Add(this.add);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Memories";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -198,19 +212,19 @@ namespace Memories
 
         #endregion
 
-        private System.Windows.Forms.Label lblWord;
-        private System.Windows.Forms.Label lblSentence;
-        private System.Windows.Forms.Label lblTranslation;
-        private System.Windows.Forms.TextBox txtWord;
-        private System.Windows.Forms.TextBox txtSentence;
-        private System.Windows.Forms.TextBox txtTranslation;
-        private System.Windows.Forms.Label lblTime;
-        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.TextBox txt_translation;
+        private System.Windows.Forms.Label lb_word;
+        private System.Windows.Forms.Label lb_sentence;
+        private System.Windows.Forms.Label lb_translation;
+        private System.Windows.Forms.TextBox txt_sentence;
+        private System.Windows.Forms.TextBox txt_word;
+        private System.Windows.Forms.Button remove;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button get;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.Button btnGet;
-        private System.Windows.Forms.Button btnRemove;
-        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lb_time;
+        private System.Windows.Forms.TextBox txt_time;
     }
 }
 
--- Memories/Form1.cs
+++ Memories/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,51 @@ namespace Memories
 {
     public partial class Form1 : Form
     {
+        DataTable table = new DataTable("Verbs");
         public Form1()
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            readTable();
+            if (table.Columns.Count < 1) createTable();
+            fillData();
+        }
+
+
+        private void readTable()
+        {
+            if (File.Exists(Application.StartupPath + "\\table_schema") && File.Exists(Application.StartupPath + "\\table_verbs"))
+            {
+                table.ReadXmlSchema(Application.StartupPath + "\\table_schema");
+                table.ReadXml(Application.StartupPath + "\\table_verbs");
+            }
+        }
+        private void createTable()
+        {
+            table.Columns.Add("Verbs", typeof(String));
+            table.Columns.Add("Sentance", typeof(String));
+            table.Columns.Add("Translation", typeof(String));
+        }
+        private void fillData()
+        {
+            // Source
+            dataGridView1.DataSource = table;
+
+            // Apperance
+            //dataGridView1.Columns["Translation"].Visible = false;
+            //dataGridView1.Columns["Sentance"].Visible = false;
+            //dataGridView1.Columns["Verbs"].Width = 285;
+        }
+        private void writeTable()
+        {
+            // table schema
+            table.WriteXmlSchema(Application.StartupPath + "\\table_schema");
+            table.WriteXml(Application.StartupPath + "\\table_verbs");
+        }
+
+        
 
         private void btnStart_Click(object sender, EventArgs e)
         {
@@ -24,5 +66,8 @@ namespace Memories
             f2.ShowDialog();
             Show();
         }
+    
+      
     }
 }
+      
